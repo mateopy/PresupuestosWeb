8d25ff1e33f7a641f796ef2af5cc5f37b9489c91
Mateo Nuñez <mateo@web.com.py>
2018-07-23 21:25:57 -0400

    Agregar conexión a Mysql y modelo Nota de Pedido

--- PresupuestosWeb/app/models.py
+++ PresupuestosWeb/app/models.py
@@ -3,5 +3,45 @@ Definition of models.
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class NotaPedido(models.Model):
+    fecha = models.DateTimeField('Fecha')
+    nroPedido = models.IntegerField()
+    usuario = models.ForeignKey(User)
+    origen = models.ForeignKey('Dependencia', related_name='dependencia_origen')
+    destino = models.ForeignKey('Dependencia', related_name='dependencia_destino')
+    precioAproximado = models.FloatField(default=0)
+    descripcionUso = models.CharField(max_length=200)
+
+
+class NotaPedido_Detalle(models.Model):
+    notaPedido = models.ForeignKey('NotaPedido')
+    cantidad = models.IntegerField()
+    articulo = models.ForeignKey('Articulo')
+
+
+class Articulo(models.Model):
+    codigo = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=50)
+    tipoArticulo = models.ForeignKey('TipoArticulo')
+    codigoBarras = models.CharField(max_length=50)
+    categoria = models.ForeignKey('CategoriaArticulo')
+    precio = models.FloatField(default=0)
+    observaciones = models.CharField(max_length=200)
+
+
+#### TABLAS MENORES ####
+class Dependencia(models.Model):
+    descripcion = models.CharField(max_length=50)
+    sucursal = models.ForeignKey('Sucursal')
+
+class Sucursal(models.Model):
+    descripcion = models.CharField(max_length=50)
+    codigo = models.IntegerField()
+
+class TipoArticulo(models.Model):
+    descripcion = models.CharField(max_length=50)
+
+class CategoriaArticulo(models.Model):
+    descripcion = models.CharField(max_length=50)
