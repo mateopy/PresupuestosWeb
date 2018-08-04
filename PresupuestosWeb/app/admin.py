ed80f7328ba9f41c5f46de15fde0449726ed2d4b
Adrian <adrianarevalos94@gmail.com>
2018-08-04 19:07:54 -0400

    Se agrega funcionalidad de poder procesar pedidos en estado borrador al listar los pedidos.

--- PresupuestosWeb/app/admin.py
+++ PresupuestosWeb/app/admin.py
@@ -2,18 +2,64 @@ from app.models import *
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.db.models import When, Case, Value, CharField
+from django.utils.html import format_html
+from django.urls import reverse, path
+from django.conf.urls import url
+from django.http import HttpResponseRedirect
 
 class NotaPedidoInLine(admin.TabularInline):
     model = NotaPedidoDetalle
     can_delete = True
     verbose_name_plural = 'Pedidos'
 
-
 class NotaPedidoAdmin(admin.ModelAdmin):
+    BORRADOR = 'B'
+    EN_PROCESO = 'E'
     #readonly_fields = ('fechaCreacion','fechaActualizacion')
-    inlines = (NotaPedidoInLine, )
-    list_display = ('nroPedido','fecha','descripcionUso','estado')
-    
+    inlines = (NotaPedidoInLine,)
+    list_display = ('nroPedido','fecha','descripcionUso','estado','accion_pedido')
+    ordering = ['nroPedido']
+    actions = ['actualizar_estado']
+
+    #list_select_related = ('usuario',)
+
+    #metodo para actualizar de forma masiva los pedidos marcados
+    def actualizar_estado(self, request, queryset):
+        filas = queryset.update(estado = self.EN_PROCESO)
+        if filas == 1:
+            mensaje = "1 Pedido Procesado"
+        else:
+            mensaje = "%s Pedidos Procesados " %filas
+        self.message_user(request, "%s Satisfacoriamente" %mensaje)
+    actualizar_estado.short_description = "Procesar Pedidos"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            #url(r'^$', app.views.home, name='home')
+            path('procesar/(<int:notapedido_id>)/', self.admin_site.admin_view(self.procesar_pedido), name='pedido_enviar')
+        ]
+        return custom_urls + urls
+
+    # metodo para agregar el boton en el list del pedido
+    def accion_pedido(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Enviar</a>&nbsp;',
+            reverse('admin:pedido_enviar', args=[obj.pk]),
+        )
+    accion_pedido.short_description = 'Procesar'
+    accion_pedido.allow_tags = True
+
+    #metodo para cambiar el estado de los pedidos en borrador
+    def procesar_pedido(self, request, notapedido_id, *args, **kwargs):
+        pedido = self.get_object(request, notapedido_id)
+        if (pedido and pedido.estado == self.BORRADOR):
+            pedido.estado = self.EN_PROCESO
+            pedido.save()
+            url = reverse('admin:app_notapedido_changelist',current_app=request.resolver_match.namespace)
+            return HttpResponseRedirect(url)
+        #return request
+
     def get_queryset(self, request):
         queryset = super(NotaPedidoAdmin, self).get_queryset(request)
         if request.user.is_superuser: return queryset
@@ -29,6 +75,7 @@ class NotaPedidoAdmin(admin.ModelAdmin):
         #        ),
         #    )
         return queryset
+
 
 class UsuarioInLine(admin.StackedInline):
     model = Usuario
