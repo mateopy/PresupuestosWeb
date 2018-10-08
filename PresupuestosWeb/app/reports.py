ca0b9fab27ca7e1929273a056ead04afba33ebe8
dahibe <dahia810@gmail.com>
2018-10-08 09:08:02 -0300

    Modificaci{on Reporte Orden Compra.

--- PresupuestosWeb/app/reports.py
+++ PresupuestosWeb/app/reports.py
@@ -13,6 +13,8 @@ from datetime import datetime
 from reportlab.pdfgen import canvas
 from PresupuestosWeb import settings
 from app.models import *
+from django.conf.locale import *
+
 
 
 
@@ -20,6 +22,7 @@ def nota_pedido_report(request,id):
     
     master = NotaPedido.objects.get(id=id)
     detail = master.notapedidodetalle_set.all()
+    cant_elementos = len(detail)
 
     response = HttpResponse(content_type = 'application/pdf')
     #pdf_name = "nota_pedido.pdf" 
@@ -28,7 +31,7 @@ def nota_pedido_report(request,id):
     buffer = BytesIO()
     #doc = getDoc(buff,'nota de pedido')
     c = canvas.Canvas(buffer, pagesize=A4)
-
+    width, height = A4
     #Header
     c.setTitle("Nota Pedido")
     c.setLineWidth(.3)
@@ -44,40 +47,65 @@ def nota_pedido_report(request,id):
 
     #Table Header
     styles = getSampleStyleSheet()
+    #styleBH = styles["BodyText"]
+    #styleBH.alignment = TA_CENTER
+    #styleBH.fontSize = 12
+
+    
+    #styleN.alignment = TA_LEFT
     styleBH = styles["Normal"]
     styleBH.alignment = TA_CENTER
     styleBH.fontSize = 12
     
     
-    parrafoStyle = ParagraphStyle('parrafos',alignment = TA_JUSTIFY,fontSize = 12,fontName="Times-Roman")
+    parrafoStyle = ParagraphStyle('parrafos',alignment = TA_JUSTIFY,fontSize = 8,fontName="Times-Roman")
     headings = ('Cantidad', 'Unidad de Medida', 'Descripción')
     results = [(d.cantidad, d.unidadMedida, d.articulo.descripcion) for d in detail]
     
     c.setFont("Times-Bold", 12)
     c.drawString(80, 740, "Para: ")
-    c.drawString(80, 720, "De: ")
+    c.drawString(270, 740, "De: ")
     c.setFont("Times-Roman", 12)
     c.drawString(110, 740, str(master.departamentoDestino))
-    c.drawString(110, 720, str(master.departamentoOrigen))
-    c.drawString(80, 700, "Solicitamos nos provean de los siguientes: ")
+    c.drawString(295, 740, str(master.departamentoOrigen))
+    c.drawString(80, 720, "Solicitamos nos provean de los siguientes: ")
 
     elements.append(headings)
     elements.append(results)
-
+    #c.setFont("Times-Roman", 6)
+    
+    
     #Detalle Tabla
-    table = Table([headings]+results, colWidths=[1.35*inch, 1.35*inch, 3.5*inch])
+    #for i in range(1, 10):
+    styles = getSampleStyleSheet()
+    styleN = styles["BodyText"]
+    table = Table([headings]+results, colWidths=[1.35*inch, 1.35*inch, 3.5*inch], rowHeights=0.18*inch,splitByRow=10)
     table.setStyle(TableStyle(
-                        [ ('GRID', (0, 0), (4, -3), 1, colors.black),
-                        ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
-                        ('BOX', (0, 0), (-1, -1), 1, colors.black),
-                        ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
-                        ('BACKGROUND', (0, 0), (-1, 0), colors.transparent),
+                        [
+                            ('VALIGN', (0,0), (- 1, -1), 'BOTTOM'),
+                            ('FONT', (0,0), (-1,0), 'Times-Bold'),
+                            ('GRID', (0, 0), (4, -3), 1, colors.black),
+                            ('FONT', (0,0), (-1,0), 'Times-Roman'),
+                            ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
+                            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+                            ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
+                            ('BACKGROUND', (0, 0), (-1, 0), colors.transparent),
+                            ('FONTSIZE', (0,0),(-1,-1), 8), 
+                            ('ALIGN', (4,0), (-1,-1), 'CENTER'),
                         ]
                     ))
 
-    elements.append(table)
-    table.wrapOn(c, 600, 500)
-    table.drawOn(c, 80, 630)
+    #elements.append(table)
+    
+    table.wrapOn(c, 600, 800)
+    table.drawOn(c, 80, 580)
+    
+
+    #table.wrapOn(c, 800, 600)
+    #if cant_elementos < 5:
+    #    table.drawOn(c, 80, y-58)
+    #else:
+    #    table.drawOn(c, 80, 400)
 
     c.setFont("Times-Bold", 12)
     c.drawString(80, 480, "Para uso en: ")
@@ -103,43 +131,52 @@ def nota_pedido_report(request,id):
     nro = c.drawString(270, 360, str(master.nroPedido))
     c.drawString(400, 380,  master.fecha.strftime('%d-%m-%Y'))
     c.drawImage(archivo_imagen, 80, 360, width=70, height=45)
+
     c.drawString(80, 340,  "Para: ")
-    c.drawString(80, 320, "De: " )
+    c.drawString(280, 340, "De: " )
     c.setFont("Times-Roman", 12)
     c.drawString(110, 340, str(master.departamentoDestino))
-    c.drawString(110, 320, str(master.departamentoOrigen))
-    c.drawString(80, 300, "Solicitamos nos provean de los siguientes: ")
+    c.drawString(300, 340, str(master.departamentoOrigen))
+    c.drawString(80, 320, "Solicitamos nos provean de los siguientes: ")
 
     #Detalle Tabla 2
-    table = Table([headings]+results, colWidths=[1.35*inch, 1.35*inch, 3.5*inch])
-    table.setStyle(TableStyle(
-                        [ ('GRID', (0, 0), (4, -3), 1, colors.black),
-                        ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
-                        ('BOX', (0, 0), (-1, -1), 1, colors.black),
-                        ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
-                        ('BACKGROUND', (0, 0), (-1, 0), colors.transparent),
+    table2 = Table([headings]+results, colWidths=[1.35*inch, 1.35*inch, 3.5*inch], rowHeights=0.20*inch,splitByRow=10)
+    table2.setStyle(TableStyle(
+                        [
+                            ('VALIGN', (0,0), (- 1, -1), 'BOTTOM'),
+                            ('FONT', (0,0), (-1,0), 'Times-Bold'),
+                            ('GRID', (0, 0), (4, -3), 1, colors.black),
+                            ('FONT', (0,0), (-1,0), 'Times-Roman'),
+                            ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
+                            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+                            ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
+                            ('BACKGROUND', (0, 0), (-1, 0), colors.transparent),
+                            ('FONTSIZE', (0,0),(-1,-1), 8), 
+                            ('ALIGN', (4,0), (-1,-1), 'CENTER'),
                         ]
                     ))
 
-    elements.append(table)
-    table.wrapOn(c, 250, 150)
-    table.drawOn(c, 80, 230)
+    #elements.append(table2)
+    table2.wrapOn(c, 800, 600)
+    table2.drawOn(c, 80, 230)
+   
+
     c.setFont("Times-Bold", 12)
-    c.drawString(80, 90, "Para uso en: ")
-    c.drawString(80, 70, "Precio aproximado: ")
+    c.drawString(80, 70, "Para uso en: ")
+    c.drawString(80, 50, "Precio aproximado: ")
     c.setFont("Times-Roman", 12)
-    c.drawString(190, 90, to_str(master.descripcionUso))
-    c.drawString(190, 70, to_str(master.precioAproximado))
+    c.drawString(190, 70, to_str(master.descripcionUso))
+    c.drawString(190, 50, to_str(master.precioAproximado))
     #elements.append(Paragraph(c.drawString(80, 55, "Precio aproximado: " + to_str(master.precioAproximado)) ,parrafoStyle))
 
-    c.line(80, 45, 220, 45)
-    c.line(270, 45, 350, 45)
-    c.line(410, 45, 500, 45)
+    c.line(80, 30, 220, 30)
+    c.line(270, 30, 350, 30)
+    c.line(410, 30, 500, 30)
     
     c.setFont("Times-Roman", 8)
-    c.drawString(85, 30, "Vo.Bo. Gte. Administrativo")
-    c.drawString(280, 30, "Jefe de Est.")
-    c.drawString(415, 30, "Gerente de Área")
+    c.drawString(85, 15, "Vo.Bo. Gte. Administrativo")
+    c.drawString(280, 15, "Jefe de Est.")
+    c.drawString(415, 15, "Gerente de Área")
     
     c.save()
     pdf = buffer.getvalue()
@@ -551,56 +588,74 @@ def presupuesto_report(request, id):
     c.setLineWidth(.3)
     c.setFont("Times-Bold", 12)
     titulo1 = c.drawString(200,790, 'SOLICITUD PRESUPUESTO')
-    #c.drawString(250, 770, )
     nro = c.drawString(250, 770, 'Nº ' + str(master.nroPresupuesto))
     c.drawString(400, 790, master.fecha.strftime('%d-%m-%Y'))
-    
     print(settings.STATIC_ROOT)
     archivo_imagen = settings.STATIC_ROOT+'/app/images/logo.png'
-    c.drawImage(archivo_imagen, 80, 760, width=70, height=45)
+    c.drawImage(archivo_imagen, 80, 760, width=65, height=50)
 
     #Table Header
     styles = getSampleStyleSheet()
-    styleBH = styles["Normal"]
+    styleBH = styles["BodyText"]
     styleBH.alignment = TA_CENTER
-    styleBH.fontSize = 8
+    styleBH.fontSize = 10
     
     
-    parrafoStyle = ParagraphStyle('parrafos',alignment = TA_JUSTIFY,fontSize = 12,fontName="Times-Roman")
-    headings = ('Cantidad', 'Unidad de Medida', 'Descripción')
-    results = [(d.cantidad, d.unidadMedida, d.articulo.descripcion) for d in detail]
+    parrafoStyle = ParagraphStyle('parrafos',alignment = TA_JUSTIFY,fontSize = 8,fontName="Times-Roman")
+    headings = ('Cantidad', 'Unidad de Medida', 'Descripción', 'Precio', 'Subtotal')
+    results = [(d.cantidad, d.unidadMedida, d.articulo.descripcion, d.precio, d.subtotal) for d in detail]
     #elements.append(tabla_encabezado(styles,titulo1,nro))
+    
     c.setFont("Times-Bold", 12)
-    #c.drawString(80, 740, "Para: ")
-    c.drawString(80, 720, "Ref. Pedido: ")
-    c.drawString(80, 705, "Proveedor.: ")
-    c.drawString(80, 690, "Fecha Entrega: ")
+    c.drawString(80, 740, "Proveedor: ")
+    c.drawString(80, 720, "Fecha Entrega: ")
+    c.drawString(350, 720, "Moneda: ")
+    c.drawString(80, 240, "Total: ")
+    c.drawString(80, 220, "Forma de Pago: ")
+    c.drawString(200, 220, "Términos y Condiciones: " )
+    c.drawString(80, 180, "Ref. Pedido: ")
+    c.drawString(80, 160, "Usuario: ")
+    
     c.setFont("Times-Roman", 12)
-    c.drawString(160, 720, str(master.pedido))
-    c.drawString(160, 705, str(master.proveedor))
-    c.drawString(160, 690, str(master.fechaEntrega))
+    c.drawString(165, 740, str(master.proveedor))
+    c.drawString(165, 720, str(master.fechaEntrega))
+    c.drawString(400, 720, str(master.moneda))
+    #c.drawString(480, 240, '{:,}'.format(master.total) )
+    c.drawString(480, 240, Decimal(master.total).quantize(Decimal('1.000')) )
+    c.drawString(80, 200, str(master.plazoPago))
+    c.drawString(330, 220, str(master.terminosCondiciones))
+    c.drawString(150, 180, str(master.pedido))
+    c.drawString(150, 160, str(master.usuario))
     
     elements.append(headings)
     elements.append(results)
 
     #Detalle Tabla
-    table = Table([headings]+results, colWidths=[1.35*inch, 1.35*inch, 3.5*inch])
+    table = Table([headings]+results, colWidths=[0.7*inch, 1.22*inch, 3.0*inch,0.8*inch,0.8*inch],rowHeights=0.18*inch)
     table.setStyle(TableStyle(
-                        [ ('GRID', (0, 0), (4, -3), 1, colors.black),
-                        ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
-                        ('BOX', (0, 0), (-1, -1), 1, colors.black),
-                        ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
-                        ('BACKGROUND', (0, 0), (-1, 0), colors.transparent),
+                        [
+                            ('VALIGN', (0,0), (- 1, -1), 'MIDDLE'),
+                            ('GRID', (0, 0), (4, -3), 1, colors.black),
+                            ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
+                            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+                            ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
+                            ('BACKGROUND', (0, 0), (-1, 0), colors.transparent),
+                            ('ALIGN', (4,0), (-1,-1), 'CENTER'),
                         ]
                     ))
 
     elements.append(table)
     table.wrapOn(c, 600, 500)
-    table.drawOn(c, 80, 600)
+    table.drawOn(c, 80, 660)
 
+    
     c.setFont("Times-Roman", 8)
-    c.line(80, 450, 210, 450)
-    c.drawString(85, 440, "Gerente de Área")
+    c.line(80, 70, 160, 70)
+    c.line(250, 70, 340, 70)
+    c.line(400, 70, 480, 70)
+    c.drawString(90, 55, "Gerente de Área")
+    c.drawString(280, 55, "Hecho por")
+    c.drawString(430, 55, "Vº Bº")
 
     
 
@@ -642,42 +697,63 @@ def ocompra_report(request, id):
     styleBH.fontSize = 8
     
     
-    parrafoStyle = ParagraphStyle('parrafos',alignment = TA_JUSTIFY,fontSize = 12,fontName="Times-Roman")
-    headings = ('Cantidad', 'Unidad de Medida', 'Descripción')
-    results = [(d.cantidad, d.unidadMedida, d.articulo.descripcion) for d in detail]
+    parrafoStyle = ParagraphStyle('parrafos',alignment = TA_JUSTIFY,fontSize = 8,fontName="Times-Roman")
+    headings = ('Cantidad', 'Unidad de Medida', 'Descripción', 'Precio', 'Subtotal')
+    results = [(d.cantidad, d.unidadMedida, d.articulo.descripcion, d.precio, d.subtotal) for d in detail]
+    #elements.append(tabla_encabezado(styles,titulo1,nro))
     
     c.setFont("Times-Bold", 12)
-    #c.drawString(80, 740, "Para: ")
-    c.drawString(80, 720, "Ref. Pedido: ")
-    c.drawString(80, 705, "Proveedor.: ")
-    c.drawString(80, 690, "Fecha Entrega: ")
+    c.drawString(80, 740, "Proveedor: ")
+    c.drawString(80, 720, "Fecha Entrega: ")
+    c.drawString(350, 720, "Moneda: ")
+    c.drawString(80, 240, "Total: ")
+    c.drawString(80, 220, "Forma de Pago: ")
+    c.drawString(200, 220, "Términos y Condiciones: " )
+    c.drawString(80, 180, "Ref. Pedido: ")
+    c.drawString(80, 160, "Usuario: ")
+    
     c.setFont("Times-Roman", 12)
-    c.drawString(160, 720, str(master.pedido))
-    c.drawString(160, 705, str(master.proveedor))
-    c.drawString(160, 690, str(master.fechaEntrega))
+    c.drawString(165, 740, str(master.proveedor))
+    c.drawString(165, 720, str(master.fechaEntrega))
+    c.drawString(400, 720, str(master.moneda))
+    #c.drawString(480, 240, '{:,}'.format(master.total) )
+    c.drawString(480, 240, Decimal(master.total).quantize(Decimal('1.000')) )
+    c.drawString(80, 200, str(master.plazoPago))
+    c.drawString(330, 220, str(master.terminosCondiciones))
+    c.drawString(150, 180, str(master.pedido))
+    c.drawString(150, 160, str(master.usuario))
     
     elements.append(headings)
     elements.append(results)
 
     #Detalle Tabla
-    table = Table([headings]+results, colWidths=[1.35*inch, 1.35*inch, 3.5*inch])
+    table = Table([headings]+results, colWidths=[0.7*inch, 1.22*inch, 3.0*inch,0.8*inch,0.8*inch],rowHeights=0.18*inch)
     table.setStyle(TableStyle(
-                        [ ('GRID', (0, 0), (4, -3), 1, colors.black),
-                        ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
-                        ('BOX', (0, 0), (-1, -1), 1, colors.black),
-                        ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
-                        ('BACKGROUND', (0, 0), (-1, 0), colors.transparent),
+                        [
+                            ('VALIGN', (0,0), (- 1, -1), 'MIDDLE'),
+                            ('GRID', (0, 0), (4, -3), 1, colors.black),
+                            ('INNERGRID', (0, 0), (-1, -1), 1, colors.black),
+                            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+                            ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
+                            ('BACKGROUND', (0, 0), (-1, 0), colors.transparent),
+                            ('ALIGN', (4,0), (-1,-1), 'CENTER'),
                         ]
                     ))
 
     elements.append(table)
     table.wrapOn(c, 600, 500)
-    table.drawOn(c, 80, 600)
+    table.drawOn(c, 80, 660)
 
+    
     c.setFont("Times-Roman", 8)
-    c.line(80, 450, 210, 450)
-    c.drawString(85, 440, "Gerente de Área")
+    c.line(80, 70, 160, 70)
+    c.line(250, 70, 340, 70)
+    c.line(400, 70, 480, 70)
+    c.drawString(90, 55, "Gerente de Área")
+    c.drawString(280, 55, "Hecho por")
+    c.drawString(430, 55, "Vº Bº")
 
+    
     
 
     c.save()
@@ -738,3 +814,4 @@ def to_str(s):
     if s is None:
         return ''
     return str(s)
+
